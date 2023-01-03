82832e3f14dc4d9834e1dcfccdcbc0f94de98c88
jang-taehyun <jth0202@naver.com>
2023-01-03 17:41:36 +0900

    Test_9

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:77d3f38c912f507213feb252bc5f7da1ebc61537f3ab184bb4621e4842cb4e89
+size 40
---
+++ Source/Awake/AwakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AwakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AWAKE_API AAwakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
