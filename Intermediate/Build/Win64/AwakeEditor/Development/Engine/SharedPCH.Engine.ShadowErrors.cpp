77a561ce5c55d2d246363492684bbbd24c396fc7
jang-taehyun <jth0202@naver.com>
2023-01-04 01:37:50 +0900

    test_10

---
+++ Intermediate/Build/Win64/AwakeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30147
+#include "D:/GraduateProject/project/Awake/Intermediate/Build/Win64/AwakeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/Awake/AwakeGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef AWAKE_AwakeGameModeBase_generated_h
+#error "AwakeGameModeBase.generated.h already included, missing '#pragma once' in AwakeGameModeBase.h"
+#endif
+#define AWAKE_AwakeGameModeBase_generated_h
+
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_SPARSE_DATA
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_RPC_WRAPPERS
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAwakeGameModeBase(); \
+	friend struct Z_Construct_UClass_AAwakeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AAwakeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Awake"), NO_API) \
+	DECLARE_SERIALIZER(AAwakeGameModeBase)
+
+
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAAwakeGameModeBase(); \
+	friend struct Z_Construct_UClass_AAwakeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AAwakeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Awake"), NO_API) \
+	DECLARE_SERIALIZER(AAwakeGameModeBase)
+
+
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAwakeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAwakeGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAwakeGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAwakeGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAwakeGameModeBase(AAwakeGameModeBase&&); \
+	NO_API AAwakeGameModeBase(const AAwakeGameModeBase&); \
+public:
+
+
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAwakeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAwakeGameModeBase(AAwakeGameModeBase&&); \
+	NO_API AAwakeGameModeBase(const AAwakeGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAwakeGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAwakeGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAwakeGameModeBase)
+
+
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Awake_Source_Awake_AwakeGameModeBase_h_12_PROLOG
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_SPARSE_DATA \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_RPC_WRAPPERS \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_INCLASS \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Awake_Source_Awake_AwakeGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_SPARSE_DATA \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Awake_Source_Awake_AwakeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> AWAKE_API UClass* StaticClass<class AAwakeGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Awake_Source_Awake_AwakeGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
